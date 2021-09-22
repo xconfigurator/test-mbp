09602dfd622a8f599d3d7c288f5f9f17de6fc23f
xconfigurator <xconfigurator@hotmail.com>
2021-09-22 14:49:11 +0800

    MyBatis-Plus 访问 sakila.actor 示例 ok。

--- src/test/java/liuyang/testmbp/service/ActorServiceTest.java
+++ src/test/java/liuyang/testmbp/service/ActorServiceTest.java
@@ -1,8 +1,16 @@
 package liuyang.testmbp.service;
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import liuyang.testmbp.entity.Actor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 /**
  * MySQL 示例数据库Sakila中的actor表
@@ -11,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @scine 2021/9/16
  */
 @SpringBootTest
+@Slf4j
 public class ActorServiceTest {
 
     @Autowired
@@ -24,6 +33,14 @@ public class ActorServiceTest {
     @Test
     void testQueryPage() {
         // Page
+        LambdaQueryWrapper<Actor> actorLambdaQueryWrapper = Wrappers.<Actor>lambdaQuery();
+        actorLambdaQueryWrapper.orderByDesc(Actor::getActorId);
 
+        Page<Actor> page = new Page<>(1, 10);
+        Page<Actor> pageData = actorService.page(page, actorLambdaQueryWrapper);
+        List<Actor> records = pageData.getRecords();
+        records.stream().forEach(System.out::println);
+
+        log.info(" pageData = {}", JSON.toJSONString(pageData));
     }
 }
